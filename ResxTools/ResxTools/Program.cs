c690638e7e2e6670caf6b922402f7c5eea21aab2
Hintee <hintea_dan@yahoo.co.uk>
2020-07-21 20:32:02 +0300

    OD: Check Reference

--- ResxTools/ResxTools/Program.cs
+++ ResxTools/ResxTools/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Xml.Linq;
 
 namespace ResxTools
@@ -12,6 +13,7 @@ namespace ResxTools
         {
             DirectoryInfo rootFolderToCheck = new DirectoryInfo(@"C:\H\Ebriza");
             FileInfo resxFileToCheck = new FileInfo(@"C:\H\Ebriza\Unicolor\UC.ControlCentre.Web.Resources\Translation.resx");
+            DirectoryInfo outputFolder = new DirectoryInfo(@"C:\Users\hinte\Downloads");
 
             XDocument doc = XDocument.Parse(File.ReadAllText(resxFileToCheck.FullName));
 
@@ -30,18 +32,62 @@ namespace ResxTools
                 .ToArray()
                 ;
 
-            FileInfo[] filesToCheck 
+            FileAndContent[] filesToCheck
                 = rootFolderToCheck
                 .EnumerateFiles("*.cs", SearchOption.AllDirectories)
                 .Where(x => !x.Name.Contains(resxFileToCheck.Name, StringComparison.InvariantCultureIgnoreCase))
+                .Select(x => new FileAndContent(x))
                 .ToArray()
                 ;
 
+            Console.WriteLine($"Checking references @ {DateTime.Now} ...");
+            foreach (TranslationInfo translationInfo in translations)
+            {
+                foreach (FileAndContent file in filesToCheck)
+                {
+                    translationInfo.IsReferenced = file.Content.Contains(translationInfo.Key, StringComparison.InvariantCulture);
+                    if (translationInfo.IsReferenced)
+                        break;
 
+                    Console.Write('.');
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"DONE Checking references @ {DateTime.Now}");
+
+            PrintResults(resxFileToCheck, translations, outputFolder);
 
             Console.WriteLine($"Done @ {DateTime.Now}");
             Console.ReadLine();
         }
+
+        private static void PrintResults(FileInfo resxFileToCheck, TranslationInfo[] results, DirectoryInfo outputFolder)
+        {
+            StringBuilder printer = new StringBuilder();
+            printer.Append(resxFileToCheck.FullName);
+            printer.AppendLine();
+            foreach (TranslationInfo result in results.OrderBy(x => x.IsReferenced))
+            {
+                printer.AppendLine($"{(result.IsReferenced ? "[REFERENCED]" : "[NOT REFERENCED]")}\t{result.Key}");
+            }
+
+            FileInfo resultFile = new FileInfo(Path.Combine(outputFolder.FullName, $"resx_refs_{DateTime.Now.ToString("yyyyMMddHHmmss")}"));
+
+            File.WriteAllText(resultFile.FullName, printer.ToString());
+        }
+    }
+
+    class FileAndContent
+    {
+        readonly Lazy<string> lazyFileContent;
+        public FileAndContent(FileInfo file)
+        {
+            File = file;
+            lazyFileContent = new Lazy<string>(() => System.IO.File.ReadAllText(File.FullName));
+        }
+
+        public FileInfo File { get; }
+        public string Content => lazyFileContent.Value;
     }
 
     class TranslationInfo
@@ -49,5 +95,7 @@ namespace ResxTools
         public string Key { get; set; }
 
         public string Language { get; set; } = "Default/EN";
+
+        public bool IsReferenced { get; set; } = false;
     }
 }
