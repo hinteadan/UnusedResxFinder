bd85b6c65b24342e4e9d1b283309e03e05c8318a
Hintee <hintea_dan@yahoo.co.uk>
2020-07-21 20:03:56 +0300

    OD: Check Reference

--- ResxTools/ResxTools/Program.cs
+++ ResxTools/ResxTools/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace ResxTools
 {
@@ -6,7 +10,44 @@ namespace ResxTools
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DirectoryInfo rootFolderToCheck = new DirectoryInfo(@"C:\H\Ebriza");
+            FileInfo resxFileToCheck = new FileInfo(@"C:\H\Ebriza\Unicolor\UC.ControlCentre.Web.Resources\Translation.resx");
+
+            XDocument doc = XDocument.Parse(File.ReadAllText(resxFileToCheck.FullName));
+
+            IEnumerable<XElement> dataNodes = doc.Descendants().Where(x => x.Name == "data");
+
+            TranslationInfo[] translations
+                = doc
+                .Descendants()
+                .Where(x => x.Name == "data")
+                .Select(x =>
+                    new TranslationInfo
+                    {
+                        Key = x.Attribute("name").Value,
+                    }
+                )
+                .ToArray()
+                ;
+
+            FileInfo[] filesToCheck 
+                = rootFolderToCheck
+                .EnumerateFiles("*.cs", SearchOption.AllDirectories)
+                .Where(x => !x.Name.Contains(resxFileToCheck.Name, StringComparison.InvariantCultureIgnoreCase))
+                .ToArray()
+                ;
+
+
+
+            Console.WriteLine($"Done @ {DateTime.Now}");
+            Console.ReadLine();
         }
+    }
+
+    class TranslationInfo
+    {
+        public string Key { get; set; }
+
+        public string Language { get; set; } = "Default/EN";
     }
 }
